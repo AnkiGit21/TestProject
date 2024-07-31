9b577a46211932d90ce9af82285ab3198272f290
Admin <Admin@DESKTOP-EHNLCK4.mshome.net>
2024-08-01 00:26:48 +0530

    master git added

--- GitProject/src/javaPackage/FirstGitProj.java
+++ GitProject/src/javaPackage/FirstGitProj.java
@@ -5,6 +5,7 @@ public class FirstGitProj {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT");
 		System.out.println("Hello testBranch");
+		System.out.println("Hello master Git");
 
 	}
 
